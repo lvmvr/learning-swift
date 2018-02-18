772d128fd7afe9401a5aacf3fc7b01699a519aab
Lamar Glenn <lvmvr.x@gmail.com>
2018-02-18 10:24:58 -0500

    Fix bug keeping from completing next question. 🐛

--- Swift-4/Udemy/iOS11 Dev Bootcamp/Quizzler-iOS11-master/Quizzler/QuestionBank.swift
+++ Swift-4/Udemy/iOS11 Dev Bootcamp/Quizzler-iOS11-master/Quizzler/QuestionBank.swift
@@ -18,7 +18,7 @@ class QuestionBank {
         // Add the Question to the list of questions
         list.append(item)
         
-        // skipping one step and just creating the quiz item inside the append function
+        // SKIPPING one step and just creating the quiz item inside the append function
         list.append(Question(text: "A slug\'s blood is green.", correctAnswer: true))
         
         list.append(Question(text: "Approximately one quarter of human bones are in the feet.", correctAnswer: true))
--- Swift-4/Udemy/iOS11 Dev Bootcamp/Quizzler-iOS11-master/Quizzler/ViewController.swift
+++ Swift-4/Udemy/iOS11 Dev Bootcamp/Quizzler-iOS11-master/Quizzler/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     //Place your instance variables here
     let allQuestions = QuestionBank()
     var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
+    
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -23,13 +25,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Loads initial first question...
         let firstQuestion = allQuestions.list[0]
         questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
+        // Simply establishes boolean values (true/ false) for each UIButton.
         // No "else" statement, since default tag # for all elements is "0"
+        // pickedAnswer set to default value of 'false'...
         if sender.tag == 1 {
             pickedAnswer = true
         } else if sender.tag == 2 {
@@ -37,6 +42,10 @@ class ViewController: UIViewController {
         }
         
         checkAnswer()
+        
+        // Next question with new question...
+        questionNumber += 1
+        questionLabel.text = allQuestions.list[questionNumber].questionText
     }
     
     
@@ -51,7 +60,8 @@ class ViewController: UIViewController {
     
     
     func checkAnswer() {
-        let correctAnswer = allQuestions.list[0].answer
+        // Pulls answer property from array item...
+        let correctAnswer = allQuestions.list[questionNumber].answer
         
         if correctAnswer == pickedAnswer {
             print("You got it!")
